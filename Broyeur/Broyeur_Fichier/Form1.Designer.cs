ea03e4123511e5690a7c58bca345033df022f243
éZekira <zekiramabanga@gmail.com>
2020-01-25 06:03:17 +0100

    Broyeur

---
+++ Broyeur/Broyeur_Fichier/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Broyeur_Fichier
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ouvrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.annulerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rétablirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outilsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personnaliserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sommaireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.indexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rechercherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.àproposdeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnBroyer = new System.Windows.Forms.Button();
+            this.grpBroyeur = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.grpBroyeur.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.editionToolStripMenuItem,
+            this.outilsToolStripMenuItem,
+            this.aideToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(325, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ouvrirToolStripMenuItem,
+            this.toolStripSeparator,
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "&Fichier";
+            // 
+            // ouvrirToolStripMenuItem
+            // 
+            this.ouvrirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ouvrirToolStripMenuItem.Image")));
+            this.ouvrirToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ouvrirToolStripMenuItem.Name = "ouvrirToolStripMenuItem";
+            this.ouvrirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.ouvrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ouvrirToolStripMenuItem.Text = "&Ouvrir";
+            this.ouvrirToolStripMenuItem.Click += new System.EventHandler(this.ouvrirToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(177, 6);
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitterToolStripMenuItem.Text = "&Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // editionToolStripMenuItem
+            // 
+            this.editionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.annulerToolStripMenuItem,
+            this.rétablirToolStripMenuItem});
+            this.editionToolStripMenuItem.Name = "editionToolStripMenuItem";
+            this.editionToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.editionToolStripMenuItem.Text = "&Edition";
+            // 
+            // annulerToolStripMenuItem
+            // 
+            this.annulerToolStripMenuItem.Name = "annulerToolStripMenuItem";
+            this.annulerToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.annulerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.annulerToolStripMenuItem.Text = "&Réecrire";
+            this.annulerToolStripMenuItem.Click += new System.EventHandler(this.annulerToolStripMenuItem_Click);
+            // 
+            // rétablirToolStripMenuItem
+            // 
+            this.rétablirToolStripMenuItem.Name = "rétablirToolStripMenuItem";
+            this.rétablirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
+            this.rétablirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rétablirToolStripMenuItem.Text = "&Broyer";
+            this.rétablirToolStripMenuItem.Click += new System.EventHandler(this.rétablirToolStripMenuItem_Click);
+            // 
+            // outilsToolStripMenuItem
+            // 
+            this.outilsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.personnaliserToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.outilsToolStripMenuItem.Name = "outilsToolStripMenuItem";
+            this.outilsToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.outilsToolStripMenuItem.Text = "&Outils";
+            // 
+            // personnaliserToolStripMenuItem
+            // 
+            this.personnaliserToolStripMenuItem.Name = "personnaliserToolStripMenuItem";
+            this.personnaliserToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.personnaliserToolStripMenuItem.Text = "&Personnaliser";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // aideToolStripMenuItem
+            // 
+            this.aideToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sommaireToolStripMenuItem,
+            this.indexToolStripMenuItem,
+            this.rechercherToolStripMenuItem,
+            this.toolStripSeparator5,
+            this.àproposdeToolStripMenuItem});
+            this.aideToolStripMenuItem.Name = "aideToolStripMenuItem";
+            this.aideToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.aideToolStripMenuItem.Text = "&Aide";
+            // 
+            // sommaireToolStripMenuItem
+            // 
+            this.sommaireToolStripMenuItem.Name = "sommaireToolStripMenuItem";
+            this.sommaireToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.sommaireToolStripMenuItem.Text = "&Sommaire";
+            // 
+            // indexToolStripMenuItem
+            // 
+            this.indexToolStripMenuItem.Name = "indexToolStripMenuItem";
+            this.indexToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.indexToolStripMenuItem.Text = "&Index";
+            // 
+            // rechercherToolStripMenuItem
+            // 
+            this.rechercherToolStripMenuItem.Name = "rechercherToolStripMenuItem";
+            this.rechercherToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.rechercherToolStripMenuItem.Text = "&Rechercher";
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(144, 6);
+            // 
+            // àproposdeToolStripMenuItem
+            // 
+            this.àproposdeToolStripMenuItem.Name = "àproposdeToolStripMenuItem";
+            this.àproposdeToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.àproposdeToolStripMenuItem.Text = "À &propos de...";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(6, 23);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(35, 13);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Path: ";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(41, 19);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(253, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(89, 45);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 3;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnBroyer
+            // 
+            this.btnBroyer.Location = new System.Drawing.Point(176, 45);
+            this.btnBroyer.Name = "btnBroyer";
+            this.btnBroyer.Size = new System.Drawing.Size(75, 23);
+            this.btnBroyer.TabIndex = 4;
+            this.btnBroyer.Text = "Broyer";
+            this.btnBroyer.UseVisualStyleBackColor = true;
+            this.btnBroyer.Click += new System.EventHandler(this.btnBroyer_Click);
+            // 
+            // grpBroyeur
+            // 
+            this.grpBroyeur.Controls.Add(this.btnBroyer);
+            this.grpBroyeur.Controls.Add(this.lblPath);
+            this.grpBroyeur.Controls.Add(this.btnBrowser);
+            this.grpBroyeur.Controls.Add(this.txtPath);
+            this.grpBroyeur.Location = new System.Drawing.Point(12, 27);
+            this.grpBroyeur.Name = "grpBroyeur";
+            this.grpBroyeur.Size = new System.Drawing.Size(335, 95);
+            this.grpBroyeur.TabIndex = 5;
+            this.grpBroyeur.TabStop = false;
+            this.grpBroyeur.Text = "Broyeur";
+            this.grpBroyeur.Enter += new System.EventHandler(this.grpBroyeur_Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 108);
+            this.Controls.Add(this.grpBroyeur);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Broyeur de fichier";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.grpBroyeur.ResumeLayout(false);
+            this.grpBroyeur.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ouvrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem annulerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rétablirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outilsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personnaliserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sommaireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem indexToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rechercherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripMenuItem àproposdeToolStripMenuItem;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnBroyer;
+        private System.Windows.Forms.GroupBox grpBroyeur;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Broyeur/Broyeur_Fichier/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Broyeur_Fichier
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void grpBroyeur_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            txtPath.Text = @"C:\Res\Biblio\CSharp\Broyeur\Broyeur_Fichier\bin\Debug";
+        }
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All files (*.*)|*.*|Txt files (*.txt)|*.txt";
+            ofd.ShowDialog();
+            txtPath.Text = ofd.FileName;
+        }
+
+        private void ouvrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All files (*.*)|*.*|Txt files (*.txt)|*.txt";
+            ofd.ShowDialog();
+            txtPath.Text = ofd.FileName;
+        }
+        private bool TableauByteVersFichierr(string nomFichier, byte[] tableauByte)
+        {
+            try
+            {
+                FileStream fs = new FileStream(nomFichier, FileMode.Create, FileAccess.Write);
+                fs.Write(tableauByte, 0, tableauByte.Length);
+                fs.Flush();
+                fs.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+            return false;
+        }
+        private void btnBroyer_Click(object sender, EventArgs e)
+        {
+            if (txtPath.Text== string.Empty)
+            {
+                MessageBox.Show("No path");
+            }
+            else
+            {
+                FileInfo file1 = new FileInfo(txtPath.Text);
+                long fileLength = file1.Length;
+                string fileExtension = file1.Extension;
+                char[] lettre = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
+                byte[] data = new byte[fileLength];
+                for (int j = 0; j < lettre.Length; j++)
+                {
+                    for (int i = 0; i < fileLength; i++)
+                    {
+                        data[i] = (byte)lettre[j];
+                    }
+                    bool resultat;
+                    resultat = TableauByteVersFichierr(txtPath.Text, data);
+                }
+                MessageBox.Show($"Ecriture terminée");
+                file1.Delete();
+                MessageBox.Show($"Fichier supprimé");
+                txtPath.Text = @" ";
+            }
+
+
+        }
+
+        private void rétablirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileInfo file1 = new FileInfo(txtPath.Text);
+            long fileLength = file1.Length;
+            string fileExtension = file1.Extension;
+            char[] lettre = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
+            byte[] data = new byte[fileLength];
+            for (int j = 0; j < lettre.Length; j++)
+            {
+                for (int i = 0; i < fileLength; i++)
+                {
+                    data[i] = (byte)lettre[j];
+                }
+                bool resultat;
+                resultat = TableauByteVersFichierr(txtPath.Text, data);
+            }
+            file1.Delete();
+            MessageBox.Show($"Fichier supprimé");
+        }
+
+        private void annulerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileInfo file1 = new FileInfo(txtPath.Text);
+            long fileLength = file1.Length;
+            string fileExtension = file1.Extension;
+            char[] lettre = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
+            byte[] data = new byte[fileLength];
+            for (int j = 0; j < lettre.Length; j++)
+            {
+                for (int i = 0; i < fileLength; i++)
+                {
+                    data[i] = (byte)lettre[j];
+                }
+                bool resultat;
+                resultat = TableauByteVersFichierr(txtPath.Text, data);
+            }
+            MessageBox.Show($"Ecriture terminée");
+        }
+    }
+}
